46afbd98514761f017f16fdd52fc8d0945e945f8
VG-PRONG <75707817+VG-PRONG@users.noreply.github.com>
2022-10-25 21:33:00 -0300

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    // Property of the floor
+    [Header("floor configuration")]
+    public float        _floordestroyed;
+    public float        _floorsize;
+    public float        _floorspeed;
+    public GameObject  _floorPrefab; 
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
