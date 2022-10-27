3d87e12ec276e6a6debd5be1702d589fc2cb7286
VG-PRONG <75707817+VG-PRONG@users.noreply.github.com>
2022-10-26 21:34:23 -0300

    Add files via upload

--- GraoundRepeating.cs
+++ GraoundRepeating.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GraoundRepeating : MonoBehaviour
 {
+
+    private Controller _controller;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _controller = FindObjectOfType(typeof(Controller)) as Controller;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        MoveChao();
+    }
+
+    void MoveChao()
+    {
+
     }
 }
