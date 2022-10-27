c2a3c7833bd04978904d733ebf6a59b3ef89451e
VG-PRONG <75707817+VG-PRONG@users.noreply.github.com>
2022-10-27 15:26:50 -0300

    Add files via upload

--- GraoundRepeating.cs
+++ GraoundRepeating.cs
@@ -6,17 +6,34 @@ public class GraoundRepeating : MonoBehaviour
 {
 
     private Controller _controller;
+    
+    public bool _floorinstanciado = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        _controller = FindObjectOfType(typeof(Controller)) as Controller;
+        _Controller = FindObjectOfType(typeof(Controller)) as Controller;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_floorinstanciado == false)
+        {
+            if(transform.position.x <= 0)
+            {
+                _floorinstanciado = true;
+                GameObject ObjectTemporaryFloor = Instantiate(_Controller._floorPrefab);
+                ObjectTemporaryFloor.transform.position = new vector3(transform.position.x + _Controller._floorsize, transform.position.y, 0);
+
+                Debug.Log("O floor was instanciado!");
+            }
+        }
+
+        if(transform.position.x < _Controller.floordestroyed) // -38
+        {
+           Destroy(this.GameObject);
+        }
     }
 
     private void FixedUpdate()
@@ -26,6 +43,6 @@ public class GraoundRepeating : MonoBehaviour
 
     void MoveChao()
     {
-
+        transform.Translate(vector2.left * _Controller._floorspeed * Time.deltaTime);
     }
 }
